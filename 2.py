57c0b715adc40d16a29719d7b4a65d198dd5416a
Y530 <berzi669@gmail.com>
2019-10-02 13:09:05 +0200

    1

--- 2.py
+++ 2.py
@@ -1,10 +1,16 @@
 class Vektor :
 
     def __init__(self, origox=0, origoy=0):
+        self.origox = 0
+        self.origoy = 0
         self.x = origox
         self.y = origoy
 
     def __str__(self):
         return "A vektor kordinátái: ({},{})".format(self.x, self.y)
 
-print(Vektor())
+    def getHossz(self):
+        return ((self.x - self.origox) ** 2 + (self.y - self.origoy) ** 2) **0.5
+
+print(Vektor(2,4))
+print(getHossz())
